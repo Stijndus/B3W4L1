9a95a74f0bded3b682553199e209a05946797cb4
Stijn <99053054@mydavinci.nl>
2021-03-15 13:04:45 +0100

    Include

---
+++ Include/content.php
@@ -0,0 +1,6 @@
+<section>
+    <img src='max.jpg' alt='Mijn super cute kat'>
+    <aside>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sequi rerum ducimus hic eveniet quas sit magnam minima alias ex ab eum officiis aspernatur nobis pariatur commodi beatae, accusantium dolorem!</p>
+    </aside>
+</section>
